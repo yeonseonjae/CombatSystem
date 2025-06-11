e998c7cba38f3d12477af54388c7b8a71546158f
yeonseonjae <ysj080822@gmail.com>
2025-06-11 23:39:33 +0900

    commits

--- src/main/java/me/shark0822/combatSystem/CombatSystem.java
+++ src/main/java/me/shark0822/combatSystem/CombatSystem.java
@@ -5,9 +5,9 @@ import me.shark0822.combatSystem.events.DamageListener;
 import me.shark0822.combatSystem.stats.StatUtil;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class CombatAPI extends JavaPlugin {
+public final class CombatSystem extends JavaPlugin {
 
-    private static CombatAPI instance;
+    private static CombatSystem instance;
 
     @Override
     public void onEnable() {
@@ -30,7 +30,7 @@ public final class CombatAPI extends JavaPlugin {
         getLogger().info("CombatAPI 플러그인이 비활성화 되었습니다.");
     }
 
-    public static CombatAPI getInstance() {
+    public static CombatSystem getInstance() {
         return instance;
     }
 }
